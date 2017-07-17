948e21a7487e83208819d4395d278fd45a6a339c
knightofknee <bricarlis@gmail.com>
2017-07-17 14:23:51 -0500

    carved some independent space

---
+++ brianshenanigans/request.js
@@ -0,0 +1,11 @@
+const parseString = require('xml2js').parseString;
+const axios = require('axios');
+
+
+axios.get('https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/FamilyMemberHistory?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB')
+.then(res => {
+  console.log(res.data);
+})
+.catch(console.error);
+
+
