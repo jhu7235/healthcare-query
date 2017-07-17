869e875d5e15cb811f9b1e0d879cf38eeed4f317
Jason <jhu7235@github.com>
2017-07-17 13:47:06 -0500

    cleaning out folders

--- healthcare-api/client/react/components/Inbox.js
+++ healthcare-api/client/react/components/Inbox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Message from './Message';
-import store from '../redux/store';
+import store from '../../redux/store';
 
 export default class extends React.Component {
 
