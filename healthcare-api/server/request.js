0059ec661eb77c9810c23e2a904b19602fec1eec
Raj <rajsekhar@Rajsekhars-MacBook-Air.local>
2017-07-17 14:26:30 -0500

    deleted trial and we can make requests now

--- healthcare-api/server/request.js
+++ healthcare-api/server/request.js
@@ -1,11 +1,8 @@
-const parseString = require('xml2js').parseString;
 const axios = require('axios');
 
-
+//axios automatically converts xml to request 
 axios.get('https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/FamilyMemberHistory?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB')
 .then(res => {
-  console.log(res.data);
+  console.log('data', res.data);
 })
 .catch(console.error);
-
-
