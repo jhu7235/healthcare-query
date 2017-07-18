f6ee261343d5a8970c51c490faad4681953f634b
knightofknee <bricarlis@gmail.com>
2017-07-18 14:23:46 -0500

    allergy component addition

--- brianshenanigans/request.js
+++ brianshenanigans/request.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-
+let allergyOutputArray = []
 axios.get('https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/AllergyIntolerance?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB')
 .then(res => {
   let allergyDataArray = res.data.entry
