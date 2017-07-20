0e87bbcc8eda14b5f991fb96ecb6c7a86851036b
Jason <jhu7235@github.com>
2017-07-20 11:00:54 -0500

    setting up db

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../user/user');
+const User = require('../../db/user/user.model');
 
 router.post('/login', (req, res, next) => {
 	User.findOne({
@@ -7,7 +7,7 @@ router.post('/login', (req, res, next) => {
 		password: req.body.password,
 	})
 		.then(user => {
-			if (!user) {res.sendStatus(401);}
+			if (!user) { res.sendStatus(401); }
 			else {
 				req.session.userId = user.id;
 				res.json(user);
@@ -15,5 +15,7 @@ router.post('/login', (req, res, next) => {
 		})
 		.catch(next);
 });
+
+// router.post('/sign')
 
 module.exports = router;
---
+++ server/db/user/user.model.js
@@ -0,0 +1,39 @@
+'use-strict';
+const db = require('../_db');
+const Sequelize = require('sequelize');
+
+const User = db.define('user', {
+  name: Sequelize.STRING,
+  photo: {
+    type: Sequelize.STRING,
+    defaultValue: '/images/default-photo.jpg'
+  },
+  phone: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  googleId: Sequelize.STRING,
+  githubId: Sequelize.STRING,
+  twitterId: Sequelize.STRING
+}, {
+  scopes: {
+    populated: () => ({
+      include: [{
+        model: db.model('story'),
+        attributes: {exclude: ['paragraphs']}
+      }]
+    })
+  }
+});
+
+module.exports = User;
